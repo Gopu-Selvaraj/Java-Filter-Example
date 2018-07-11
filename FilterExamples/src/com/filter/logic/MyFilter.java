4e1d1c7139cf4574495f3f774cdc982251299616
Gopu Selvaraj <38911042+Gopu-Selvaraj@users.noreply.github.com>
2018-07-11 11:02:55 +0530

    Add files via upload

---
+++ FilterExamples/src/com/filter/logic/MyFilter.java
@@ -0,0 +1,37 @@
+package com.filter.logic;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.*;
+
+public class MyFilter implements Filter{
+
+	public void init(FilterConfig arg0) throws ServletException {}
+	
+	public void doFilter(ServletRequest req, ServletResponse resp,
+			FilterChain chain) throws IOException, ServletException {
+		
+		System.out.println("Filter Condition Run First");
+		
+		
+		PrintWriter out=resp.getWriter();
+		
+		String password=req.getParameter("password");
+		if(password.equals("admin")){
+		chain.doFilter(req, resp);//sends request to next resource
+		System.out.println("Filter Condition is true");
+		}
+		else{
+		out.print("Password Invalid!!!");
+		// Filter is always running first
+		// Filter Logic first check then next
+		// Filter Logic true then enter servlet
+		System.out.println("Filter Condition is false!!! so redirect to index");
+		RequestDispatcher rd=req.getRequestDispatcher("index.html");
+		rd.include(req, resp);
+		}
+		
+	}
+	public void destroy() {}
+   //https://www.javatpoint.com/authentication-filter
+	}
